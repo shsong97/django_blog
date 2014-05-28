2040e73536c5adb1628e33c7781f9fade36b664f
shsong97 <shsong97@gmail.com>
2014-05-28 09:28:53 +0900

    eclipse setting change

--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,8 @@
 # -.- coding: UTF-8 -.-
 from django.http import HttpResponse
 def home(request):
-  html_text="<html><head><title>Django Site Home</title></head>\
-  <body><ul><li><a href='/blog'>블로그</a>\
-  <li><a href='/polls'>투표</a>\
-  </ul></body></html>"
-  return HttpResponse(html_text)
+    html_text="<html><head><title>Django Site Home</title></head>\
+    <body><ul><li><a href='/blog'>블로그</a>\
+    <li><a href='/polls'>투표</a>\
+    </ul></body></html>"
+    return HttpResponse(html_text)
