13b5e809513597e3a12c1b1a8182c1bb1de4e562
Seunghun,Song <shsong97@gmail.com>
2014-06-03 06:37:28 +0000

    commit html

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,11 +4,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'mysite.views.home', name='home'),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls',namespace="blog")),
     url(r'^polls/', include('polls.urls',namespace="polls")),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^timeline/', include('timeline.urls',namespace="timeline")),
     url(r'^contact/', 'mysite.views.contact', name='contact'),
 )
 
---
+++ timeline/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, url
+from timeline import views
+
+urlpatterns = patterns('',
+    url(r'^api/timeline/$',views.timeline_view),
+    url(r'^api/timeline/create/$',views.message_create_view),
+    url(r'^api/timeline/(?P<num>\d+)/$',views.message_view),
+    url(r'^api/timeline/(?P<num>\d+)/delete/$',views.message_delete_view),
+    url(r'^api/timeline/(?P<num>\d+)/like/$',views.like_view),
+    url(r'^api/timeline/find/$',views.find_view),                       
+    url(r'^api/user/(?P<method>create)/$',views.user_view),
+    url(r'^api/user/(?P<method>update)/$',views.user_view),
+    url(r'^api/user/(?P<method>list)/$',views.user_view),
+    url(r'^api/user/name/$',views.name_view),
+    url(r'^api/user/checkpassword/$',views.checkpassword_view),
+    url(r'^api/user/setpassword/$',views.setpassword_view),
+    url(r'^api/profile/(?P<username>\w+)/$',views.profile_view),
+    url(r'^api/profile/$',views.profile_view),
+    url(r'^api/login/$',views.login_view),
+    url(r'^home/(?P<page>\w+).html/$',views.serve_html),
+
+)
